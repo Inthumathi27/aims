0ae7c638de520e872f2715f64c0c5994fc55e642
Inthumathi <inthumathi@medcubics.com>
2022-11-14 10:06:55 +0530

    Orientation imported

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:aims/UI%20screens/Splash%20Screen/splashscreen.dart';
 import 'package:aims/UI%20screens/leave/applyleave.dart';
 import 'package:aims/utils/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
